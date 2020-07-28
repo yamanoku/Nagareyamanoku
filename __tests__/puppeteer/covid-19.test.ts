86841e8f362ae47b1807c552524ea05ffb4ff3a1
yamanoku <0910yama@gmail.com>
2020-07-28 20:29:23 +0900

    update

--- __tests__/puppeteer/covid-19.test.ts
+++ __tests__/puppeteer/covid-19.test.ts
@@ -7,7 +7,7 @@ describe('COVID-19-Test', () => {
     await expect(page).toMatchElement('#voice > h2 + p');
   })
 
-  it('「流山市内居住者において新型コロナウイルス感染症の患者」の文字列が存在するか', async () => {
-    await expect(page).toMatch('流山市内居住者において新型コロナウイルス感染症の患者')
+  it('「流山市内居住者において新型コロナウイルス感染症の感染者」の文字列が存在するか', async () => {
+    await expect(page).toMatch('流山市内居住者において新型コロナウイルス感染症の感染者')
   })
 })
